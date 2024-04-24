b22dfe01ca6a3cb82d2f2ff553ffe3d75f7c84e9
Justin <jrussel1055@gmail.com>
2024-04-24 11:42:35 -0500

    cleanup

--- fractal/gateway/apps.py
+++ fractal/gateway/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
 
 
 class GatewayConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "fractal.gateway"
-
-    # def ready(self):
-    #     from fractal.gateway.signals import create_
-
-    #     post_migrate.connect(create_matrix_homeserver_for_default_target, sender=self)
